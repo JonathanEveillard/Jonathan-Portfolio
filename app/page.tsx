0cbcca71f7517bf880750f42b9d113e36ed74751
Jonathan <jonathaneveillard@cmail.carleton.ca>
2024-07-28 19:17:03 -0400

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,6 @@ import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
 import Approach from "@/components/Approach";
-import Experience from "@/components/Experience";
-import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
