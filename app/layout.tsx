09adb8a41c753f22313e4d4db597795cc05b46af
Jonathan <jonathaneveillard@cmail.carleton.ca>
2024-07-28 19:07:13 -0400

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jonathan's Portfolio",
+  description: "Improving the world one line of code at a time",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+       <link rel="icon" href="/code.png" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ components/Footer.tsx
@@ -0,0 +1,41 @@
+import { FaLocationArrow } from "react-icons/fa6";
+import MagicButton from "./MagicButton";
+
+const Footer = () => {
+  return (
+    <footer className="w-full pt-20 pb-10" id="contact">
+      {/* background grid */}
+      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+        <img
+          src="/footer-grid.svg"
+          alt="grid"
+          className="w-full h-full opacity-50 "
+        />
+      </div>
+
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          We improve ourselves with the best <span className="text-purple">people</span>
+        </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          I am always open to new opportunities and meeting new people. Feel free to reach out to me
+        </p>
+        <a href="mailto:jonaeve50@gmail.com">
+          <MagicButton
+            title="Let's get in touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Jonathan. All rights reserved.
+        </p>
+
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
